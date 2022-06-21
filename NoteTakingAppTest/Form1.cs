57717d5dfed3f69ed1f997c13d11434c9dc27d0f
teodor-matei <teodor.matei@student.unitbv.ro>
2022-06-21 15:53:33 +0300

    added database connection, added insert/delete operations

--- NoteTakingAppTest/Form1.cs
+++ NoteTakingAppTest/Form1.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using System.Data.SqlClient;
 
 namespace NoteTakingAppTest
 {
@@ -12,15 +13,34 @@ namespace NoteTakingAppTest
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            String connectionString = "Data Source=DESKTOP-464DU9K;Initial Catalog=POO;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Notes",connection);
+
+            notes = new DataTable();
+            adapter.Fill(notes);
+
+            notes.Columns[0].ColumnName = "ID";
+            notes.Columns[1].ColumnName = "Title";
+            notes.Columns[2].ColumnName = "Content";
+            notes.Columns[3].ColumnName = "Date";
+
+            /*
             notes = new DataTable();
             notes.Columns.Add("Title", typeof(String));
             notes.Columns.Add("Content", typeof(String));
             notes.Columns.Add("Date", typeof(String));
+            */
 
             dataGridView1.DataSource = notes;
+            dataGridView1.Columns["ID"].Visible = false;
             dataGridView1.Columns["Content"].Visible = false;
             dataGridView1.Columns["Title"].Width = 160;
             dataGridView1.Columns["Date"].Width = 63;
+
+            connection.Close();
         }
 
         private void btnNew_Click(object sender, EventArgs e)
@@ -36,8 +56,25 @@ namespace NoteTakingAppTest
         {
             if (!String.IsNullOrEmpty(textTitle.Text) && !String.IsNullOrEmpty(textContent.Text))
             {
+                String connectionString = "Data Source=DESKTOP-464DU9K;Initial Catalog=POO;Integrated Security=True";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
                 String date = DateTime.Now.ToShortDateString();
-                notes.Rows.Add(textTitle.Text, textContent.Text, date);
+                //notes.Rows.Add(textTitle.Text, textContent.Text, date);
+
+                SqlCommand command = new SqlCommand("INSERT INTO Notes (Title,Data,Date) VALUES (@Title,@Content,@Date)", connection);
+                command.Parameters.AddWithValue("@Title", textTitle.Text);
+                command.Parameters.AddWithValue("@Content", textContent.Text);
+                command.Parameters.AddWithValue("Date", date);
+                command.ExecuteNonQuery();
+
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Notes", connection);
+                notes.Clear();
+                adapter.Fill(notes);
+                dataGridView1.DataSource = notes;
+
+                connection.Close();
 
                 textTitle.Clear();
                 textContent.Clear();
@@ -50,8 +87,8 @@ namespace NoteTakingAppTest
 
             if (index > -1)
             {
-                textTitle.Text = notes.Rows[index].ItemArray[0].ToString();
-                textContent.Text = notes.Rows[index].ItemArray[1].ToString();
+                textTitle.Text = notes.Rows[index].ItemArray[1].ToString();
+                textContent.Text = notes.Rows[index].ItemArray[2].ToString();
                 textTitle.Enabled = false;
                 textContent.Enabled = false;
             }
@@ -61,7 +98,22 @@ namespace NoteTakingAppTest
         {
             int index = dataGridView1.CurrentCell.RowIndex;
 
-            notes.Rows[index].Delete();
+            String connectionString = "Data Source=DESKTOP-464DU9K;Initial Catalog=POO;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("DELETE Notes WHERE ID=@ID", connection);
+            command.Parameters.AddWithValue("@ID", notes.Rows[index].ItemArray[0]);
+            command.ExecuteNonQuery();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Notes", connection);
+            notes.Clear();
+            adapter.Fill(notes);
+            dataGridView1.DataSource = notes;
+
+            connection.Close();
+
+            //notes.Rows[index].Delete();
         }
     }
 }
